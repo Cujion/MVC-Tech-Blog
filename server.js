babd12fd74b2c8eaaf036a8031e04b43acfb6a36
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-18 15:39:34 -0500

    added starter files

---
+++ server.js
@@ -0,0 +1,36 @@
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`listen at http://localhost:${PORT}`);
+    });
+});
