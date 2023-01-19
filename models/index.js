eb58be972595ce29616a0920d20b646d3669b3da
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-18 19:25:10 -0500

    updated model index for relationships between each model

--- models/index.js
+++ models/index.js
@@ -3,23 +3,32 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-
+    foreignKey: 'user_id',
 });
 
 Post.hasMany(Comment, {
-
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
 });
 
 User.hasMany(Comment, {
-
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
 });
 
 Post.belongsTo(User, {
-
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
 });
 
 
