f134553e1710364c4c5d7f4267754df1e21031dd
Chris <106836962+Cujion@users.noreply.github.com>
2023-01-18 19:51:10 -0500

    added starter code for home-routes.js

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 200000,
+    maxAge: 300000,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
@@ -38,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
